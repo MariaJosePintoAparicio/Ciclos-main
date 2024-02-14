bddd43b9b2a7f96b29f2c5a5fb2f01f7ec24eebc
MariaJosePintoAparicio <152021983+MariaJosePintoAparicio@users.noreply.github.com>
2024-02-14 08:22:22 -0500

    hola

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
-let lista = new Array(11).fill(undefined);
-for(let i in lista){
-    console.group(`Table del ${i}`);
-    for(let x in lista){
-        console.log(`${i} X ${x} = ${Number(i)*Number(x)}`);
-    }
-    console.groupEnd("");
-}
-
+//let lista = new Array(11).fill(undefined);
+//for(let i in lista){
+//    console.group(`Table del ${i}`);
+//    for(let x in lista){
+//        console.log(`${i} X ${x} = ${Number(i)*Number(x)}`);
+//    }
+//    console.groupEnd("");
+//}
+//
 
 // let list = new Array(10).fill(undefined);
 
@@ -19,4 +19,25 @@ for(let i in lista){
 //     console.groupEnd("");
 // }
 
+//let data = prompt ("ingrese el nombre")
+//console.log(Data);
+//
+//parseInt()
+//parseFloat()
+//
+//Number()
+//
+//let bandera = confirm("estoy entendiendo java script")
+//console.log(bandera);
+//
 
+let lista = ["papa" , "yuca" , "cabolla" , "arroz"]
+while(confirm("¿ deseas selecccionar un producto?")) {
+    let plantilla = "";
+    for (let i in lista){
+    plantilla += `${Number(i)+1}. ${lista[i]}\n`;
+
+}
+    let opc = Number(prompt(`selecccione un producto de la lista:\n${plantilla}`));
+    alert(`el producto que se selecciono es: ${lista[opc-1]}`)
+}
